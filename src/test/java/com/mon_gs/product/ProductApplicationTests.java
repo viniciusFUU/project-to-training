7cff8bea530fe36f0af1376486d46ad848d6b094
Vinícius Monteiro <vinicius3_1998@hotmail.com>
2025-06-21 00:36:16 -0300

    Init

---
+++ src/test/java/com/mon_gs/product/ProductApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mon_gs.product;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProductApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
